cf68e4547c682e42c7052211148cbeb53cd3d09f
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-07-31 22:38:35 +0900

    #4.0 Props

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,7 +1,19 @@
 function App() {
+    function Btn({text,big}) {
+      console.log({text,big});
+      return <button style={{
+        backgroundColor: 'tomato',
+        color: 'white',
+        padding: "10px 20px",
+        border: 0,
+        borderRadius: 10,
+        fontSize: big ? 16 : 12
+      }}>{text}</button>
+    }
   return (
     <>
-      
+    <Btn text="Save Changes" big={true}/>
+    <Btn text="Continue" big={false}/>
     </>
   );
 }
