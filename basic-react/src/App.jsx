abeedda5cc964bbd0c5affaae751318402ac66b2
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-02 03:51:26 +0900

    #6.0 introduction

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
+
 function App() {
+    const [counter, setValue] = useState(0);
+    const onClick = () => setValue((prev)=> prev + 1);
+    console.log("call an api");
   return (
     <div>
+        <h1>{counter}</h1>
+        <button onClick={onClick}>click me!</button>
     </div>
   );
 }
