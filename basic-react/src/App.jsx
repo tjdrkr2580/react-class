2e22bf8313dc6577b83383c76b6606979fe995aa
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-01 23:08:38 +0900

    #5.0 Introduction setting

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,20 +1,6 @@
-import { useState } from "react";
-
 function App() {
-  const [value,setValue] = useState("Save Changes");
-    function Btn({text, fontSize}) {
-      return <button style={{
-        backgroundColor: 'tomato',
-        color: 'white',
-        padding: "10px 20px",
-        border: 0,
-        borderRadius: 10,
-        fontSize,
-      }}>{text}</button>
-    }
   return (
     <div>
-    <Btn text={value} fontSize={18}/>
     </div>
   );
 }
