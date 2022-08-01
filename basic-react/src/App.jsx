b3548318546b8a0b5a4e8bc1fb734bf84421b654
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-02 04:03:12 +0900

    #6.1 useEffect

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
     const [counter, setValue] = useState(0);
     const onClick = () => setValue((prev)=> prev + 1);
-    console.log("call an api");
+    console.log("i run all the time");
+    const iRunOnlyOnce = () => {
+      console.log("i run only once");
+    }
+    useEffect(iRunOnlyOnce,[]);
   return (
     <div>
         <h1>{counter}</h1>
