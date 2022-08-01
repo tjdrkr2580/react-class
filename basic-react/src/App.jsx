b268ad33d589db083e762a9a5950573f0c13fe20
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-01 18:21:41 +0900

    #4.2 PropTypes

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -2,20 +2,20 @@ import { useState } from "react";
 
 function App() {
   const [value,setValue] = useState("Save Changes");
-    function Btn({text}) {
-      console.log({text});
+    function Btn({text, fontSize}) {
       return <button style={{
         backgroundColor: 'tomato',
         color: 'white',
         padding: "10px 20px",
         border: 0,
         borderRadius: 10,
+        fontSize,
       }}>{text}</button>
     }
   return (
-    <>
-    <Btn text={value}/>
-    </>
+    <div>
+    <Btn text={value} fontSize={18}/>
+    </div>
   );
 }
 
