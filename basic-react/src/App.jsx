8ed7966c6fa073acb40f0b55dc2dda2fa09f7750
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-02 04:22:18 +0900

    #6.4 Cleanup setting

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,19 +1,9 @@
 import { useEffect, useState } from 'react';
 
 function App() {
-    const [counter, setValue] = useState(0);
-    const [keyword, setKeyword] = useState("");
-    const onClick = () => setValue((prev)=> prev + 1);
-    const onChange = (e) => setKeyword(e.target.value);
-    console.log("i run all the time");
-    useEffect(()=>{
-      console.log("run now, couter keyword");
-    },[keyword,counter]);
   return (
     <div>
-        <input type="text" value={keyword} onChange={onChange} placeholder='Search Here...' />
-        <h1>{counter}</h1>
-        <button onClick={onClick}>click me!</button>
+      
     </div>
   );
 }
