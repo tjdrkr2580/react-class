3812701b64b84137f91ea48e1f5ef390f5f05285
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-01 13:25:02 +0900

    #4.1 Memo setting

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,19 +1,18 @@
 function App() {
-    function Btn({text,big}) {
-      console.log({text,big});
+    function Btn({text}) {
+      console.log({text});
       return <button style={{
         backgroundColor: 'tomato',
         color: 'white',
         padding: "10px 20px",
         border: 0,
         borderRadius: 10,
-        fontSize: big ? 16 : 12
       }}>{text}</button>
     }
   return (
     <>
-    <Btn text="Save Changes" big={true}/>
-    <Btn text="Continue" big={false}/>
+    <Btn text="Save Changes"/>
+    <Btn text="Continue"/>
     </>
   );
 }
