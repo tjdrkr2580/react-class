5e51609aea52124b90ea9c61449c63d1b22a720a
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-04 07:00:26 +0900

    #7.0 To Do List Part One

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,12 +1,29 @@
-import { useEffect, useState } from 'react';
-
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div>
-     
-    </div>
-  );
+    const [toDo, setToDo] = useState("");
+    const [toDos, setTodos] = useState([]);
+
+    const onChange = (e) => {
+        setToDo(e.target.value);
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (toDo === '') {
+            return;
+        }
+        setTodos((currentArray) => [toDo, ...currentArray])
+        setToDo("");
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h1>My To Dos ({toDos.length})</h1>
+            <input type="text" onChange={onChange} value={toDo}/>
+            <button>Add To Do</button>
+        </form>
+    );
 }
 
 export default App;
