ba825981d7b73a1dba11c11ad114d8dc9f37ccc3
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-04 07:36:01 +0900

    #7.2 Coin Tracker

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,11 +1,25 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-
+    const [loading, setLoading] = useState(true);
+    const [coins,setCoins] = useState([]);
+    useEffect(()=> {
+      fetch("https://api.coinpaprika.com/v1/tickers").then((response) => response.json()).then((json) => { setCoins(json); setLoading(false)});
+    },[]);
 
     return (
       <div>
-      
+      <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
+      <select>
+        {
+          loading ? "" :
+          coins.map((coin) => (
+            <option>
+             {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
+            </option>
+          ))
+        }
+      </select>
   
       </div>
 
