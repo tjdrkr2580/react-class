25b061b96d1dba4795e3b125bbc7a0b21c6f8696
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-04 07:15:01 +0900

    #7.2 Coin Tracker

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,35 +1,12 @@
 import {useState} from 'react';
 
 function App() {
-    const [toDo, setToDo] = useState("");
-    const [toDos, setTodos] = useState([]);
 
-    const onChange = (e) => {
-        setToDo(e.target.value);
-    }
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (toDo === '') {
-            return;
-        }
-        setTodos((currentArray) => [toDo, ...currentArray])
-        setToDo("");
-    }
 
     return (
       <div>
-        <form onSubmit={onSubmit}>
-            <h1>My To Dos ({toDos.length})</h1>
-            <input type="text" onChange={onChange} value={toDo}/>
-            <button>Add To Do</button>
-        </form>
-        <hr />
-        <ul>
-          {
-            toDos.map((item) => (<li>{item}</li>))
-          }
-        </ul>
+      
+  
       </div>
 
     );
