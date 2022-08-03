eba941648eccaeeb03b40441cc16dbd63a3094f0
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-03 14:38:33 +0900

    #7.0 To Do List Part One setting

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,24 +1,10 @@
 import { useEffect, useState } from 'react';
 
-const Hello = () => {
-  const byfn = () => {
-    console.log("bye :(");
-  }
-  const hifn = () => {
-    console.log("hi :)");
-    return byfn
-  }
-  useEffect(hifn,[]);
-  return <h1>Hello</h1>
-}
 
 function App() {
-  const [showing,setShowing] = useState(false);
-  const onClick = () => setShowing(!showing);
   return (
     <div>
-      {showing ? <Hello/> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+     
     </div>
   );
 }
