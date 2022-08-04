576edfb82fa1d98e95e9200777974529229a929d
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-04 18:28:27 +0900

    to do list re commit

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -6,25 +6,26 @@ function App() {
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
-    if (toDo === "") {
+    if(toDo === ''){
       return;
     }
     setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
     console.log(toDos);
-  };
+  }
+
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
+      <h1>My Daily To Dos {toDos.length}</h1>
       <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
+        <input type="text" onChange={onChange} value={toDo} />
+        <button>To Do!</button>
       </form>
+      <ul>
+        {
+          toDos.map((item) => (<li>{item}</li>))
+        }
+      </ul>
     </div>
   );
 }
