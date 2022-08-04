aae3fc5e7979610903c9d0a1af7f27d8c4af7642
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-04 18:16:03 +0900

    to do list one 비동기식 -> 동기식 변환해보기

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -1,29 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState } from "react";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [coins,setCoins] = useState([]);
-    useEffect(()=> {
-      fetch("https://api.coinpaprika.com/v1/tickers").then((response) => response.json()).then((json) => { setCoins(json); setLoading(false)});
-    },[]);
-
-    return (
-      <div>
-      <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
-      <select>
-        {
-          loading ? "" :
-          coins.map((coin) => (
-            <option>
-             {coin.name} ({coin.symbol}): ${coin.quotes.USD.price}
-            </option>
-          ))
-        }
-      </select>
-  
-      </div>
-
-    );
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+    console.log(toDos);
+  };
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
